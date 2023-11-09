44befe9b834a8a27ed3ae3353e56409eb8f03a37
Zekai Koç <zekaikoc@gmail.com>
2023-11-09 14:36:41 +0100

    assignment 1 added.

---
+++ Assignment_1_IF_COLL/assign_if.py
@@ -0,0 +1,9 @@
+# Task : Let's say you left a message before that prints a password you need. To see the password you left, you need to enter your name and the program should recognize you.
+# Write a program that 
+
+# Takes the first name from the user and compares it to yours,
+# Then if the name the user entered is the same as yours, print out such as : "Hello, Joseph! The password is : @12",
+# If the name the user entered is not the same as yours, print out such as : "Hello, Amina! See you later."
+# Write your Python codes on any IDLE, push it up to your GitHub repository and submit your GitHub Page link address in addition to your code as a plain text.
+
+
