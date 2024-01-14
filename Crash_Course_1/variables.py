e224f50b678f7f00b25c7c7a58d0769040d282ad
Zekai Koç <zekaikoc@gmail.com>
2024-01-14 23:56:19 +0100

    variables refresh

--- Crash_Course_1/variables.py
+++ Crash_Course_1/variables.py
@@ -1,10 +1,29 @@
-var = 22
+# var = 22
 
-print(var)
+# print(var)
 
-x = 5
-y = 77
+# x = 5
+# y = 77
 
-print(x + y)
-y = 1
-print(x + y)
+# print(x + y)
+# y = 1
+# print(x + y)
+
+# my_string = "Hello, World!"
+
+# print(my_string)
+
+num = 11
+name = "John"
+
+# print('My name is {}, and my favorite number is {}'.format(name, num))
+
+# print('My name is {one}, and my favorite number is {two}'.format(one=name, two=num))
+
+# print(name[1])
+# print(name[0])
+print(f'My name is {name}, and my favorite number is {num}')
+
+my_list = [1, 2, 3, [4, 5, 6, 'test'], 7, 8, 9]
+
+print(my_list[3][3][3])
