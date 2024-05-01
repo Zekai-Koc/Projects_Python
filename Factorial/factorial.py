d05f66382b34a6c6689145a189cf228533a94c59
Zekai Koç <zekaikoc@gmail.com>
2024-05-01 17:18:37 +0200

    other way.

--- Factorial/factorial.py
+++ Factorial/factorial.py
@@ -1,15 +1,21 @@
 
-def factorial(number):
+# def factorial(number):
+#    if number == 0: return 1
+#    return number * factorial(number - 1)
+
+# print(factorial(5))
+
+
+# def factorial2(number):
+#    result = 1
+#    for i in range(1, number + 1):
+#       result *= i
+#    return result
+
+# print(factorial2(5))
+
+def myFactorial(number):
    if number == 0: return 1
-   return number * factorial(number - 1)
+   return number * myFactorial(number - 1)
 
-print(factorial(5))
-
-
-def factorial2(number):
-   result = 1
-   for i in range(1, number + 1):
-      result *= i
-   return result
-
-print(factorial2(5))
+print(myFactorial(5))
